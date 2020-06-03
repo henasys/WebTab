8f48052464f8e309c942642f1d48932b199b8dad
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-06-03 12:13:54 +0900

    add handlers for remote message

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {Alert} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 import Tabs from './src/screens/Tabs';
 
 export default function App() {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
   return (
     <NavigationContainer>
       <Tabs />
