74d3753018e7cca12a14c14b68e0bfe5f9579b81
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-06-05 12:44:25 +0900

    add handlers for WebView

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import {StyleSheet, SafeAreaView} from 'react-native';
+import React, {useRef} from 'react';
+import {StyleSheet, SafeAreaView, View, Platform} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 export default function HomeScreen() {
+  const webview = useRef(null);
+  const url = 'https://m.naver.com/';
   return (
     <SafeAreaView style={styles.container}>
       <WebView
-        source={{uri: 'https://www.naver.com/'}}
+        ref={webview}
+        source={{uri: url}}
         style={styles.webView}
+        renderError={(errorDomain, errorCode, errorDesc) => {
+          console.log('WebView renderError', errorDomain, errorCode, errorDesc);
+          return <View />;
+        }}
+        onError={(syntheticEvent) => {
+          const {nativeEvent} = syntheticEvent;
+          console.log('WebView onError', Platform.OS, nativeEvent);
+        }}
+        onLoad={(syntheticEvent) => {
+          const {nativeEvent} = syntheticEvent;
+          console.log('WebView onLoad', Platform.OS, nativeEvent.url);
+        }}
+        onHttpError={(syntheticEvent) => {
+          const {nativeEvent} = syntheticEvent;
+          console.log('WebView onHttpError', Platform.OS, nativeEvent);
+        }}
+        // onShouldStartLoadWithRequest={(request) => {
+        //   console.log(
+        //     'WebView onShouldStartLoadWithRequest',
+        //     Platform.OS,
+        //     request,
+        //   );
+        //   return true;
+        // }}
+        onNavigationStateChange={(navState) => {
+          console.log(
+            'WebView onNavigationStateChange',
+            Platform.OS,
+            navState.loading,
+            navState.url,
+          );
+        }}
+        onFileDownload={({nativeEvent}) => {
+          const {downloadUrl} = nativeEvent;
+          console.log('WebView onFileDownload', Platform.OS, downloadUrl);
+          console.log(nativeEvent);
+        }}
       />
     </SafeAreaView>
   );
